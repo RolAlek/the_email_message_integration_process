2526b2254d54df7426579a31820b8ea69329aca6
Aleksandr Rol <rolaleksandr75@gmail.com>
2024-08-11 23:27:13 +0900

    django channel

---
+++ the_message_integration_project/static/js/email_script.js
@@ -0,0 +1,20 @@
+const socket = new WebSocket('ws://localhost:8080/ws/emails/');
+
+socket.onmessage = function(event) {
+    const data = JSON.parse(event.data);
+
+    if (data.progress) {
+        document.getElementById('progress-text').innerText = data.progress;
+        document.getElementById('progress-bar').value = data.percentage;
+    }
+
+    if (data.new_email) {
+        const table = document.getElementById('email-table').getElementsByTagName('tbody')[0];
+        const row = table.insertRow();
+
+        row.insertCell(0).innerText = data.new_email.message_id;
+        row.insertCell(1).innerText = data.new_email.send_date;
+        row.insertCell(2).innerText = data.new_email.subject;
+        row.insertCell(3).innerText = data.new_email.text + '...';
+    }
+}
